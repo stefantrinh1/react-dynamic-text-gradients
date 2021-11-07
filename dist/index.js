ad0ddb121e45c046ff76f54123528d62f05d2f52
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-11-07 15:45:08 -0700

    bumping version number

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,9 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _AccessibilityWidget = _interopRequireDefault(require("./components/AccessibilityWidget"));
+var _TextGradient = _interopRequireDefault(require("./components/TextGradient"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _default = _AccessibilityWidget.default;
+var _default = _TextGradient.default;
 exports.default = _default;
